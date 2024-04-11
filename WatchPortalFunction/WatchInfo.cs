3ce9e03d31da4a8cd48fc8f3de116b289b37945a
vigilija <vigilija.simkeviciute@gmail.com>
2024-04-11 23:38:47 +0200

    Add project files.

---
+++ WatchPortalFunction/WatchInfo.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace WatchPortalFunction
+{
+    public static class WatchInfo
+    {
+        private static readonly ILogger _logger;
+
+        //public WatchInfo(ILogger<WatchInfo> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [Function("WatchInfo")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+           // return new OkObjectResult("Welcome to Azure Functions!");
+           string model = req.Query["model"];
+
+            if (model == null)
+            {
+                dynamic watchinfo = new { Manufacturer = "abc", CaseType = "Solid", Bazel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+                return (ActionResult)new ObjectResult($"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bazel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}");
+            }
+            return new BadRequestObjectResult("Please provide a watch model in the query string");
+        }
+    }
+}
